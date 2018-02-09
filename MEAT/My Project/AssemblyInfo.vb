a62fe67c42a6a394792af56d7e8136e4278f4309
connor1993 <manuellugo1993@gmail.com>
2018-02-09 16:55:18 -0700

    se corrigio el rfc en cuentas x pagas/compcatproveedor

--- MEAT/My Project/AssemblyInfo.vb
+++ MEAT/My Project/AssemblyInfo.vb
@@ -34,8 +34,8 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 
-<Assembly: AssemblyVersion("1.0.57.57")> 
-<Assembly: AssemblyFileVersion("1.0.57.57")> 
+<Assembly: AssemblyVersion("1.0.60.60")> 
+<Assembly: AssemblyFileVersion("1.0.60.60")> 
 
 
 <Assembly: NeutralResourcesLanguageAttribute("")> 
