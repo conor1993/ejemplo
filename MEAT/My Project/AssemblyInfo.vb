ba051848e651fafd0b019ca47925140ed51c0b0f
connor1993 <manuellugo1993@gmail.com>
2018-02-23 13:44:59 -0700

    version

--- MEAT/My Project/AssemblyInfo.vb
+++ MEAT/My Project/AssemblyInfo.vb
@@ -34,8 +34,8 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 
-<Assembly: AssemblyVersion("1.0.70.70")> 
-<Assembly: AssemblyFileVersion("1.0.70.70")> 
+<Assembly: AssemblyVersion("1.0.71.71")> 
+<Assembly: AssemblyFileVersion("1.0.71.71")> 
 
 
 <Assembly: NeutralResourcesLanguageAttribute("")> 
