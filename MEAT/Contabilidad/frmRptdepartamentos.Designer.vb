24c45f663b36a477f87a3437289c27d93a9e5bd9
unknown <mdorantes@integra-lab.com.mx>
2018-03-22 14:30:43 -0700

    Cambios al reporte de departamentos

--- MEAT/Contabilidad/frmRptdepartamentos.Designer.vb
+++ MEAT/Contabilidad/frmRptdepartamentos.Designer.vb
@@ -22,8 +22,99 @@ Partial Class frmRptdepartamentos
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.cmbDepartamentos = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.DateTimePicker2 = New System.Windows.Forms.DateTimePicker()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'cmbDepartamentos
+        '
+        Me.cmbDepartamentos.FormattingEnabled = True
+        Me.cmbDepartamentos.Location = New System.Drawing.Point(89, 35)
+        Me.cmbDepartamentos.Name = "cmbDepartamentos"
+        Me.cmbDepartamentos.Size = New System.Drawing.Size(275, 21)
+        Me.cmbDepartamentos.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(71, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Centro Costo:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(35, 87)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Inicial:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(289, 128)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Imprimir"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DateTimePicker1.Location = New System.Drawing.Point(89, 81)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(93, 20)
+        Me.DateTimePicker1.TabIndex = 6
+        '
+        'DateTimePicker2
+        '
+        Me.DateTimePicker2.Checked = False
+        Me.DateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DateTimePicker2.Location = New System.Drawing.Point(270, 81)
+        Me.DateTimePicker2.Name = "DateTimePicker2"
+        Me.DateTimePicker2.Size = New System.Drawing.Size(94, 20)
+        Me.DateTimePicker2.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(221, 87)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(32, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Final:"
+        '
+        'frmRptdepartamentos
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(398, 168)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.DateTimePicker2)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmbDepartamentos)
+        Me.Name = "frmRptdepartamentos"
         Me.Text = "frmRptdepartamentos"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents cmbDepartamentos As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents DateTimePicker2 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Label4 As System.Windows.Forms.Label
 End Class
--- MEAT/Contabilidad/frmRptdepartamentos.vb
+++ MEAT/Contabilidad/frmRptdepartamentos.vb
@@ -1,3 +1,59 @@
-﻿Public Class frmRptdepartamentos
+﻿Imports HC = Integralab.ORM.HelperClasses
+Imports System.Data.SqlClient
 
+Public Class frmRptdepartamentos
+    Private departamento As ClasesNegocio.DepartamentosClass
+    Private DepartamentosC As New ClasesNegocio.DepartamentosCollectionClas
+
+#Region "Eventos"
+    Private Sub frmRptdepartamentos_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        DateTimePicker1.Format = DateTimePickerFormat.Custom
+        DateTimePicker1.CustomFormat = "dd/MM/yyyy"
+        Try
+            'datos.Columns.Add("Cve_Depto")
+            'datos.Columns.Add("Nom_Depto")
+            'datos.Rows.Add(-1, "TODOS")
+
+            cmbDepartamentos.DataSource = Nothing
+            cmbDepartamentos.ValueMember = "Codigo"
+            cmbDepartamentos.DisplayMember = "Nom_Depto"
+            Me.DepartamentosC.Obtener()
+            'Dim dep = New ClasesNegocio.DepartamentosCollectionClas()
+            'DepartamentosC.Add(dep)
+            Me.cmbDepartamentos.DataSource = DepartamentosC
+        Catch ex As Exception
+
+        End Try
+
+        
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        'Dim datos As New DataSet
+        Dim datos As New DataTable
+        Dim departamentoId As Integer = 0
+
+        If Not cmbDepartamentos.SelectedValue = -1 And Not cmbDepartamentos.SelectedValue Is Nothing Then
+            departamentoId = cmbDepartamentos.SelectedValue
+        End If
+
+        Dim query = "EXEC  rptdepartamentos " + departamentoId.ToString
+        
+        Using connection As New SqlConnection(HC.DbUtils.ActualConnectionString)
+            Dim adapter As New SqlDataAdapter()
+            adapter.SelectCommand = New SqlCommand(query, connection)
+            adapter.Fill(datos)
+        End Using
+
+        Dim Reporte As New ClasesNegocio.rptDepartamentos2
+
+        Dim pre As New ClasesNegocio.PreVisualizarForm
+        'Reporte.SetDataSource(datos.Tables(0))
+        Reporte.SetDataSource(datos)
+
+        pre.Reporte = Reporte
+        pre.ShowDialog()
+    End Sub
+
+#End Region
 End Class
