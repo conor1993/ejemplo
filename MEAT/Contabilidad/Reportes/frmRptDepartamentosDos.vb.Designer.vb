05be2dd728b2b59d3962e6f5d9c2549906cb848d
Dorantes <mdorantes@integra-lab.com.mx>
2018-04-05 16:41:50 -0600

    Se termina de disenio de reporte de departamentos

--- MEAT/Contabilidad/Reportes/frmRptDepartamentosDos.vb.Designer.vb
+++ MEAT/Contabilidad/Reportes/frmRptDepartamentosDos.vb.Designer.vb
@@ -22,6 +22,7 @@ Partial Class frmRptDepartamentosDos
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.lblDepartamento = New System.Windows.Forms.Label()
         Me.cmbDepartamento = New System.Windows.Forms.ComboBox()
         Me.cmbMeses = New System.Windows.Forms.ComboBox()
@@ -30,15 +31,17 @@ Partial Class frmRptDepartamentosDos
         Me.lblAnio = New System.Windows.Forms.Label()
         Me.chbComparativo = New System.Windows.Forms.CheckBox()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.rdbSimplificado = New System.Windows.Forms.RadioButton()
         Me.rdbDetallado = New System.Windows.Forms.RadioButton()
+        Me.rdbSimplificado = New System.Windows.Forms.RadioButton()
+        Me.MEAToolBar1 = New MEAToolBar.MEAToolBar()
+        Me.lblTitulo = New System.Windows.Forms.Label()
         Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'lblDepartamento
         '
         Me.lblDepartamento.AutoSize = True
-        Me.lblDepartamento.Location = New System.Drawing.Point(12, 23)
+        Me.lblDepartamento.Location = New System.Drawing.Point(64, 36)
         Me.lblDepartamento.Name = "lblDepartamento"
         Me.lblDepartamento.Size = New System.Drawing.Size(77, 13)
         Me.lblDepartamento.TabIndex = 0
@@ -47,7 +50,7 @@ Partial Class frmRptDepartamentosDos
         'cmbDepartamento
         '
         Me.cmbDepartamento.FormattingEnabled = True
-        Me.cmbDepartamento.Location = New System.Drawing.Point(91, 20)
+        Me.cmbDepartamento.Location = New System.Drawing.Point(143, 33)
         Me.cmbDepartamento.Name = "cmbDepartamento"
         Me.cmbDepartamento.Size = New System.Drawing.Size(203, 21)
         Me.cmbDepartamento.TabIndex = 1
@@ -55,7 +58,7 @@ Partial Class frmRptDepartamentosDos
         'cmbMeses
         '
         Me.cmbMeses.FormattingEnabled = True
-        Me.cmbMeses.Location = New System.Drawing.Point(91, 47)
+        Me.cmbMeses.Location = New System.Drawing.Point(143, 60)
         Me.cmbMeses.Name = "cmbMeses"
         Me.cmbMeses.Size = New System.Drawing.Size(203, 21)
         Me.cmbMeses.TabIndex = 3
@@ -63,7 +66,7 @@ Partial Class frmRptDepartamentosDos
         'lblMeses
         '
         Me.lblMeses.AutoSize = True
-        Me.lblMeses.Location = New System.Drawing.Point(12, 50)
+        Me.lblMeses.Location = New System.Drawing.Point(64, 63)
         Me.lblMeses.Name = "lblMeses"
         Me.lblMeses.Size = New System.Drawing.Size(41, 13)
         Me.lblMeses.TabIndex = 2
@@ -72,7 +75,7 @@ Partial Class frmRptDepartamentosDos
         'cmbAnio
         '
         Me.cmbAnio.FormattingEnabled = True
-        Me.cmbAnio.Location = New System.Drawing.Point(91, 74)
+        Me.cmbAnio.Location = New System.Drawing.Point(143, 87)
         Me.cmbAnio.Name = "cmbAnio"
         Me.cmbAnio.Size = New System.Drawing.Size(114, 21)
         Me.cmbAnio.TabIndex = 5
@@ -80,7 +83,7 @@ Partial Class frmRptDepartamentosDos
         'lblAnio
         '
         Me.lblAnio.AutoSize = True
-        Me.lblAnio.Location = New System.Drawing.Point(12, 77)
+        Me.lblAnio.Location = New System.Drawing.Point(64, 90)
         Me.lblAnio.Name = "lblAnio"
         Me.lblAnio.Size = New System.Drawing.Size(29, 13)
         Me.lblAnio.TabIndex = 4
@@ -89,7 +92,7 @@ Partial Class frmRptDepartamentosDos
         'chbComparativo
         '
         Me.chbComparativo.AutoSize = True
-        Me.chbComparativo.Location = New System.Drawing.Point(15, 104)
+        Me.chbComparativo.Location = New System.Drawing.Point(67, 117)
         Me.chbComparativo.Name = "chbComparativo"
         Me.chbComparativo.Size = New System.Drawing.Size(85, 17)
         Me.chbComparativo.TabIndex = 6
@@ -100,23 +103,12 @@ Partial Class frmRptDepartamentosDos
         '
         Me.GroupBox1.Controls.Add(Me.rdbDetallado)
         Me.GroupBox1.Controls.Add(Me.rdbSimplificado)
-        Me.GroupBox1.Location = New System.Drawing.Point(13, 134)
+        Me.GroupBox1.Location = New System.Drawing.Point(65, 147)
         Me.GroupBox1.Name = "GroupBox1"
         Me.GroupBox1.Size = New System.Drawing.Size(281, 70)
         Me.GroupBox1.TabIndex = 7
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Mostrar por"
-        '
-        'rdbSimplificado
-        '
-        Me.rdbSimplificado.AutoSize = True
-        Me.rdbSimplificado.Location = New System.Drawing.Point(20, 29)
-        Me.rdbSimplificado.Name = "rdbSimplificado"
-        Me.rdbSimplificado.Size = New System.Drawing.Size(81, 17)
-        Me.rdbSimplificado.TabIndex = 0
-        Me.rdbSimplificado.TabStop = True
-        Me.rdbSimplificado.Text = "Simplificado"
-        Me.rdbSimplificado.UseVisualStyleBackColor = True
         '
         'rdbDetallado
         '
@@ -129,11 +121,67 @@ Partial Class frmRptDepartamentosDos
         Me.rdbDetallado.Text = "Detallado"
         Me.rdbDetallado.UseVisualStyleBackColor = True
         '
+        'rdbSimplificado
+        '
+        Me.rdbSimplificado.AutoSize = True
+        Me.rdbSimplificado.Location = New System.Drawing.Point(20, 29)
+        Me.rdbSimplificado.Name = "rdbSimplificado"
+        Me.rdbSimplificado.Size = New System.Drawing.Size(81, 17)
+        Me.rdbSimplificado.TabIndex = 0
+        Me.rdbSimplificado.TabStop = True
+        Me.rdbSimplificado.Text = "Simplificado"
+        Me.rdbSimplificado.UseVisualStyleBackColor = True
+        '
+        'MEAToolBar1
+        '
+        Me.MEAToolBar1.ButtonSize = New System.Drawing.Size(50, 50)
+        Me.MEAToolBar1.Dock = System.Windows.Forms.DockStyle.None
+        Me.MEAToolBar1.DropDownArrows = True
+        Me.MEAToolBar1.HabilitarBorrar = True
+        Me.MEAToolBar1.HabilitarBuscar = True
+        Me.MEAToolBar1.HabilitarCancelar = True
+        Me.MEAToolBar1.HabilitarEditar = True
+        Me.MEAToolBar1.HabilitarGuardar = True
+        Me.MEAToolBar1.HabilitarImprimir = True
+        Me.MEAToolBar1.HabilitarLimpiar = True
+        Me.MEAToolBar1.HabilitarNuevo = True
+        Me.MEAToolBar1.HabilitarSalir = True
+        Me.MEAToolBar1.ImageButtonSize = MEAToolBar.MEAToolBar.ImageButtonSizeEnum.de32
+        Me.MEAToolBar1.Location = New System.Drawing.Point(1, -1)
+        Me.MEAToolBar1.MostrarBorrar = False
+        Me.MEAToolBar1.MostrarBuscar = False
+        Me.MEAToolBar1.MostrarCancelar = False
+        Me.MEAToolBar1.MostrarEditar = False
+        Me.MEAToolBar1.MostrarGuardar = False
+        Me.MEAToolBar1.MostrarImprimir = True
+        Me.MEAToolBar1.MostrarLimpiar = True
+        Me.MEAToolBar1.MostrarNuevo = False
+        Me.MEAToolBar1.MostrarSalir = True
+        Me.MEAToolBar1.Name = "MEAToolBar1"
+        Me.MEAToolBar1.ShowToolTips = True
+        Me.MEAToolBar1.Size = New System.Drawing.Size(50, 266)
+        Me.MEAToolBar1.TabIndex = 40
+        '
+        'lblTitulo
+        '
+        Me.lblTitulo.BackColor = System.Drawing.Color.SteelBlue
+        Me.lblTitulo.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitulo.ForeColor = System.Drawing.Color.White
+        Me.lblTitulo.Location = New System.Drawing.Point(66, -1)
+        Me.lblTitulo.Margin = New System.Windows.Forms.Padding(0)
+        Me.lblTitulo.Name = "lblTitulo"
+        Me.lblTitulo.Size = New System.Drawing.Size(417, 29)
+        Me.lblTitulo.TabIndex = 45
+        Me.lblTitulo.Text = "Reporte de Departamentos"
+        Me.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
         'frmRptDepartamentosDos
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(310, 220)
+        Me.ClientSize = New System.Drawing.Size(371, 231)
+        Me.Controls.Add(Me.lblTitulo)
+        Me.Controls.Add(Me.MEAToolBar1)
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.chbComparativo)
         Me.Controls.Add(Me.cmbAnio)
@@ -160,4 +208,6 @@ Partial Class frmRptDepartamentosDos
     Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
     Friend WithEvents rdbDetallado As System.Windows.Forms.RadioButton
     Friend WithEvents rdbSimplificado As System.Windows.Forms.RadioButton
+    Friend WithEvents MEAToolBar1 As MEAToolBar.MEAToolBar
+    Friend WithEvents lblTitulo As System.Windows.Forms.Label
 End Class
